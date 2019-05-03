57d5165154155146457f4217082b3c10b9073bea
Jasmyne Boggs <Jasmyne.Boggs@vu.com>
2019-05-03 15:23:47 -0500

    solution

--- Program.cs
+++ Program.cs
@@ -8,7 +8,24 @@ namespace ConnectToWebServer
     {
         static void Main(string[] args)
         {
-            TcpClient client = new TcpClient();
+            var client = new TcpClient();
+
+            const int MAXBUF = 4096;
+            const string IP = "127.0.0.1";
+            const int PORT = 8080;
+
+            Console.WriteLine("Client Started");
+            IPAddress addr = IPAddress.Parse(IP);
+            client.Connect(addr, PORT);
+            var stream = client.GetStream();
+
+
+
+
+
+
+
+
         }
     }
 }
